7b68410b9cef632fbd0056d83c0a560081e768eb
Brandon Baker <brandonbaker40@icloud.com>
2018-10-31 09:04:00 -0700

    sketches post-create component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatButtonModule, MatCardModule, MatToolbarModule, MatIconModule, MatMenuModule } from '@angular/material';
+import { MatButtonModule, MatCardModule, MatToolbarModule, MatIconModule, MatMenuModule, MatInputModule } from '@angular/material';
+
 import { NavbarComponent } from './navbar/navbar.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    PostCreateComponent
   ],
   imports: [
     BrowserModule,
@@ -21,7 +26,9 @@ import { NavbarComponent } from './navbar/navbar.component';
     MatCardModule,
     MatToolbarModule,
     MatIconModule,
-    MatMenuModule
+    MatMenuModule,
+    MatInputModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
